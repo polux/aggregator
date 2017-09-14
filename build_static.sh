2b05a97c07aa29a2945cc1fe379974032341a33d
Paul Brauner <polux@google.com>
2017-09-14 13:46:47 +0200

    compile static binary with -threaded option

--- build_static.sh
+++ build_static.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stack ghc -- -outputdir out -O2 --make -static -optc-static -optl-static -optl-pthread Aggregator.hs
+stack ghc -- -threaded -outputdir out -O2 --make -static -optc-static -optl-static -optl-pthread Aggregator.hs
